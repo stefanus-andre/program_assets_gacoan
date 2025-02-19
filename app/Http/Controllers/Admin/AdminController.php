774a0d6f5980ba15eafa86eb7338d3f2fa36085e
jayaper <jayabagasper@gmail.com>
2025-02-19 16:11:24 +0700

    Dashboard Report

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -21,7 +21,56 @@ class AdminController extends Controller
 {
     public function Index()
     {
-        return view("Admin.dashboard_admin");
+
+        $total_asset = DB::table('t_transaction_qty')
+        ->sum('qty');
+
+        $bad_asset = DB::table('t_transaction_qty')
+        ->whereIn('condition', [2, 4])
+        ->sum('qty');
+
+
+        $good_asset = DB::table('t_transaction_qty')
+        ->whereIn('condition', [1, 3])
+        ->sum('qty');
+
+
+        $total_resto = DB::table('master_resto_v2')
+        ->count();
+
+        return view("Admin.dashboard_admin", [
+            'totalAsset' => $total_asset,
+            'badAsset' => $bad_asset,
+            'goodAsset' => $good_asset,
+            'totalResto' => $total_resto
+        ]);
+    }
+
+    public function getDataResto(Request $request)
+    {
+        $dataQuery = DB::table('t_transaction_qty')
+        
+        ->join('m_assets', 't_transaction_qty.asset_id', '=', 'm_assets.asset_id')
+
+        ->join('master_resto_v2', 't_transaction_qty.from_loc', '=', 'master_resto_v2.id')
+
+        ->join('t_out', 't_transaction_qty.out_id', '=', 't_out.out_id')
+        
+        ->join('m_condition', 't_transaction_qty.condition', '=', 'm_condition.condition_id')
+
+        ->select('master_resto_v2.id AS id_resto','master_resto_v2.name_store_street', 'm_assets.asset_model', 'm_condition.condition_name', 'm_condition.condition_id', 't_transaction_qty.qty', 't_transaction_qty.out_id', 't_transaction_qty.created_at');
+
+        if ($request->filled('start_date') && $request->filled('end_date')) {
+            $startDate = $request->input('start_date') . ' 00:00:00'; 
+            $endDate = $request->input('end_date') . ' 23:59:59'; 
+            $dataQuery->whereBetween('t_transaction_qty.created_at', [$startDate, $endDate]);
+        }
+
+        // Execute the query and get the paginated result
+        
+        $data = $dataQuery->get();
+
+        return response()->json($data);
     }
 
     public function HalamanAsset() {
