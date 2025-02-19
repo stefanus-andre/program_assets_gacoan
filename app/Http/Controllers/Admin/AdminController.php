60981cb0b798adbee2780ac59594d1bb39a2a0ca
jayaper <jayabagasper@gmail.com>
2025-02-19 17:52:40 +0700

    update versi lokal

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\DB;
 
 
 
